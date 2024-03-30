1d25da2161d02361e6fe842fed1c419483fa2522
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-03-30 22:57:48 +0900

    updated: country index, modal, and added daisyUI theme

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -48,6 +48,27 @@
                 {{ $slot }}
             </main>
 
+            
+
+<footer class="">
+  <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {{ Carbon\Carbon::now()->format('Y') }} <a href="https://sacode.web.id/" class="hover:underline">SaCode Community</a>.
+  </span>
+  <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+      <li>
+          <a href="https://github.com/janzenfaidiban/laravel-livewire-plygrounds" target="_blank" class="hover:underline me-4 md:me-6">Main Repository</a>
+      </li>
+      <li>
+          <a href="https://github.com/janzenfaidiban/laravel-livewire-plygrounds/blob/main/README.md" target="_blank" class="hover:underline me-4 md:me-6">README.md</a>
+      </li>
+      <li>
+          <a href="https://github.com/janzenfaidiban/laravel-livewire-plygrounds/graphs/contributors" target="_blank" class="hover:underline me-4 md:me-6">Contributors</a>
+      </li>
+  </ul>
+  </div>
+</footer>
+
+
 
               <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
                 <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
