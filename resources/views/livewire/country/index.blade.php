997e3c19adb00f5d4c986adb8d06ef889e2773c9
ellyansek18 <ellyansek18@gmail.com>
2024-04-05 01:48:16 +0700

    On Progres

--- resources/views/livewire/country/index.blade.php
+++ resources/views/livewire/country/index.blade.php
@@ -64,7 +64,7 @@
                 <label for="flag" class="block mb-2 text-sm font-medium text-gray-900">Flag</label>
                 <input wire:model="flag" type="text" id="flag" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"  required />
             </div>
-            <button type="submit" x-on:click="openModalEdit=false" class="mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            <button type="submit" x-on:click="openModalEdit=false" class="mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
         </form>
     </x-modalAlpine>
 
@@ -79,7 +79,7 @@
                 <label for="flag" class="block mb-2 text-sm font-medium text-gray-900">Flag</label>
                 <input wire:model="flag" type="text" id="flag" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"  required />
             </div>
-            <button type="submit" x-on:click="openModalCreate=false" class="mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            <button type="submit" x-on:click="openModalCreate=false" class="mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
         </form>
     </x-modalAlpine>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,17 @@ use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('laravel/country');
+    return redirect('login');
 });
 
 // Laravel Routes
 require_once('laravel.php');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
