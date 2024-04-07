6f5f85db4fd7a689eb54dcdd21b895825c3583f1
obipranata <obiimanuelpranata@gmail.com>
2024-04-07 21:53:13 +0700

    feat: add logout button

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <body>
 
         <div class="bg-white">
-          
+
             <div class="relative isolate px-6 pt-14 lg:px-8">
               <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
                 <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
@@ -41,14 +41,23 @@
                         <span class="badge badge-sm">{{ $countShops }}</span>
                       </a>
                     </li>
+                    <li x-data>
+                      <a href="#" x-on:click="$refs.form.submit()" class="hover:bg-gray-50">
+                        <i class="fa-solid fa-unlock"></i>
+                          Logout
+                      </a>
+                        <form class="hidden" x-ref="form" action="{{ route('logout') }}" method="POST">
+                            @csrf
+                        </form>
+                    </li>
                   </ul>
             </nav>
-    
+
             <main class="container mx-auto px-4">
                 {{ $slot }}
             </main>
 
-            
+
 
 <footer class="">
   <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
@@ -75,8 +84,8 @@
               </div>
             </div>
           </div>
-          
-        
-        
+
+
+
     </body>
 </html>
