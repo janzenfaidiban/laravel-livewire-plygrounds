33cfd3fb472171f3ba93e05b72c47d8f3b2f4c55
obipranata <obiimanuelpranata@gmail.com>
2024-04-07 21:58:29 +0700

    feat: add middleware auth:sanctum

--- routes/livewire.php
+++ routes/livewire.php
@@ -4,8 +4,14 @@ use App\Livewire\Country\Index as Country;
 use App\Livewire\City\Index as City;
 use App\Livewire\Shop\Index as Shop;
 
-Route::prefix('livewire')->name('livewire.')->group(function () {
-    Route::get('/countries', Country::class)->name('countries');
-    Route::get('/cities', City::class)->name('cities');
-    Route::get('/shops', Shop::class)->name('shops');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::prefix('livewire')->name('livewire.')->group(function () {
+        Route::get('/countries', Country::class)->name('countries');
+        Route::get('/cities', City::class)->name('cities');
+        Route::get('/shops', Shop::class)->name('shops');
+    });
 });
