1b3745a2e9eb9e44a645150cc1651497699f1bc8
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-04-08 21:57:07 +0900

    fix: added @vite('resources/css/app.css') to laravel app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ ucfirst(Request::segment(1) . ' - ' ?? '') }} Laravel Playground</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>SaCode - Livewire Playground</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
 
