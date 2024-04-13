519c8cca218bf963b6724027cfa366fbd55d8e49
obipranata <obiimanuelpranata@gmail.com>
2024-04-13 17:41:56 +0700

    fix: bug tags-input component doesnt work in laravel

--- resources/views/components/input-tags.blade.php
+++ resources/views/components/input-tags.blade.php
@@ -37,14 +37,16 @@
     >
         <div class="flex gap-2 border border-gray-400 min-h-10 rounded-md p-2 flex-wrap cursor-text items-center" @click="isTagsOpen = !isTagsOpen">
             <template x-for="item in tagsAdded">
-                <input type="hidden" x-bind:value="item" name="tags[]">
-                <div class="text-sm p-2 px-2 items-center text-white text-left bg-[#202142] rounded-sm flex gap-2.5">
-                    <span x-text="item"></span>
-                    <button type="button" @click="removeTags(item)">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                            <path d="M7 7L17 17M7 17L17 7" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                    </button>
+                <div>
+                    <input type="hidden" x-bind:value="item" name="tags[]">
+                    <div class="text-sm p-2 px-2 items-center text-white text-left bg-[#202142] rounded-sm flex gap-2.5">
+                        <span x-text="item"></span>
+                        <button type="button" @click="removeTags(item)">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path d="M7 7L17 17M7 17L17 7" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                    </div>
                 </div>
             </template>
             <template x-if="tagsAdded.length === 0">
